285e7a6d78f0019a510e47210806a611c795294b
Vincent Tiu <46623413+vincetiu8@users.noreply.github.com>
2024-07-09 20:53:08 -0400

    add intellisense and catch2 (#7)

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,8 @@
 #include "booker/booker.h"
 #include "concierge/concierge.h"
 #include <iostream>
+#include <catch2/catch_test_macros.hpp>
+#include <boost/beast.hpp>
 
 int main() {
     std::cout << booker() << concierge() << "\n";
